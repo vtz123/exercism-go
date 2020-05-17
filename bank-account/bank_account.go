38b8b4c16b4b8bb354840fea86c82072338b10de
Celaeno <lanchetz@gamil.com>
2020-05-17 17:56:38 +0800

    bank-store

---
+++ bank-account/bank_account.go
@@ -0,0 +1,65 @@
+package account
+
+import (
+	"sync"
+)
+
+type Account struct {
+	price	int64 
+	mu		sync.Mutex
+
+	closed  bool
+} 
+
+func Open(initialDeposit int64) *Account { 
+	if initialDeposit < 0 {
+		return nil
+	}
+
+	return &Account{
+		price:	initialDeposit, 
+		closed:	false,
+	}
+}
+
+func (a *Account) Close() (payout int64, ok bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock() 
+	opend := a.closed 
+	
+	if opend {
+		return 0, false
+	}
+
+	a.closed = true 
+
+	return a.price, true
+}
+
+func (a *Account) Balance() (balance int64, ok bool) {
+	a.mu.Lock() 
+	defer a.mu.Unlock()
+
+	opend := a.closed 
+	money := a.price
+	if opend {
+		return 0, false
+	}
+
+	return money, true
+}
+
+// 存款
+func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	a.mu.Lock() 
+	defer a.mu.Unlock()
+
+	opend := a.closed 
+	
+	if opend ||  a.price + amount < 0{
+		return 0, false
+	}
+
+	a.price += amount
+	return a.price, true
+}
